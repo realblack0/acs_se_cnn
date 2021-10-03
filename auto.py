607f8936c6ff5a65be66d4324750bd2379bb7179
jinhyo <realblack0@gmail.com>
2021-10-03 23:43:26 +0900

    remove cache, update .gitignore

---
+++ auto.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--main', required=True)
+parser.add_argument('--name', required=True)
+parser.add_argument('--device', default="cuda")
+parser.add_argument('--nohupNN', required=True)
+parser.add_argument('--seed', default="n")
+args = parser.parse_args()
+
+for subject_id in range(1,10):
+    CODE = f"nohup python -u {args.main} --name nohup{args.nohupNN}{subject_id}_{args.name}_subject{subject_id} --subject {subject_id} --device {args.device} --seed {args.seed}" \
+         + f" > nohup{args.nohupNN}{subject_id}_{args.name}_subject{subject_id}.out"
+    print(CODE)
+    os.system(CODE)
+    
+print("finish!")
